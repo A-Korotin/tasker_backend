a475b0462744c2b9cd20fcbafd11927c3a0724a4
Alexey Korotin <sx12fx@gmail.com>
2023-06-30 14:51:56 +0300

    feat: Project domain class + migration

---
+++ src/main/java/com/korotin/tasker/domain/Project.java
@@ -0,0 +1,31 @@
+package com.korotin.tasker.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * One of the domain classes of Tasker application.
+ * Represents a container with records. Should be owned by certain user.
+ * @see Record
+ * @see User
+ */
+@Entity
+@Table(name = "project")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Project extends BaseEntity {
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany
+    @JoinColumn(name = "project_id")
+    private List<Task> tasks;
+
+    @OneToMany
+    @JoinColumn(name = "project_id")
+    private List<Event> events;
+}
