ce78fa33f55a20148d49b9ab72ae85da37cea6ed
Alexey Korotin <sx12fx@gmail.com>
2023-07-02 16:16:10 +0300

    feat: default user is now being added at application startup test@test.com qwerty ADMIN

---
+++ src/main/java/com/korotin/tasker/config/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package com.korotin.tasker.config;
+
+import com.korotin.tasker.domain.User;
+import com.korotin.tasker.domain.UserRole;
+import com.korotin.tasker.repository.UserRepository;
+import com.korotin.tasker.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig implements CommandLineRunner {
+
+    private final UserService userService;
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.findByEmail("test@test.com").isPresent()) {
+            return;
+        }
+
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setPassword("qwerty");
+        user.setName("Test user");
+        user.setRole(UserRole.ADMIN);
+
+        userService.register(user);
+    }
+}
