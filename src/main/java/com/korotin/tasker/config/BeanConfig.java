833f1850097250693cc885c29c04d15add20f4b6
Alexey Korotin <sx12fx@gmail.com>
2023-07-02 16:14:47 +0300

    fix: default Locale is now english

--- src/main/java/com/korotin/tasker/config/BeanConfig.java
+++ src/main/java/com/korotin/tasker/config/BeanConfig.java
@@ -3,6 +3,10 @@ package com.korotin.tasker.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * General bean configuration class
@@ -12,5 +16,10 @@ public class BeanConfig {
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new FixedLocaleResolver(Locale.ENGLISH);
     }
 }
