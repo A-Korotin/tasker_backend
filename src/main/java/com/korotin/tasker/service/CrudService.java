9fbac8a2f35912e62080f5046e2c5b7223ef21c9
Alexey Korotin <sx12fx@gmail.com>
2023-07-03 19:42:30 +0300

    docs: added Javadoc for UserService and CrudService

--- src/main/java/com/korotin/tasker/service/CrudService.java
+++ src/main/java/com/korotin/tasker/service/CrudService.java
@@ -1,7 +1,15 @@
 package com.korotin.tasker.service;
 
-import java.util.Optional;
+import com.korotin.tasker.domain.User;
 
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Abstract service with basic CRUD operations
+ * @param <T> Service entity type (e.g. {@link User})
+ * @param <ID> Service entity ID type (e.g. {@link Long} or {@link UUID})
+ */
 public interface CrudService<T, ID> {
     T save(T entity);
     T update(ID id, T entity);
--- src/main/java/com/korotin/tasker/service/UserService.java
+++ src/main/java/com/korotin/tasker/service/UserService.java
@@ -7,6 +7,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Service for {@link User} entity.
+ * Contains basic CRUD functionality extended with business-specific methods.
+ * @see CrudService
+ * @see UserDetailsService
+ */
 public interface UserService extends CrudService<User, UUID>, UserDetailsService {
     Optional<User> findByEmail(String email);
 }
