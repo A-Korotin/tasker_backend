eac51a704f0fb250c41fb1f374d77ed1a0bf9b00
Alexey Korotin <sx12fx@gmail.com>
2023-07-03 16:49:50 +0300

    feat: User DTO builder for tests

--- src/main/java/com/korotin/tasker/domain/dto/UserDTO.java
+++ src/main/java/com/korotin/tasker/domain/dto/UserDTO.java
@@ -6,9 +6,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * General input User DTO
@@ -16,6 +14,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class UserDTO {
 
     @NotNull
