11e8c5156de3dedeb625f6c7dfebb908dd2d7bf8
Alexey Korotin <sx12fx@gmail.com>
2023-07-28 22:06:00 +0300

    feat: Task crud

---
+++ src/main/java/com/korotin/tasker/controller/TaskController.java
@@ -0,0 +1,60 @@
+package com.korotin.tasker.controller;
+
+import com.korotin.tasker.domain.Task;
+import com.korotin.tasker.domain.dto.OutputTaskDTO;
+import com.korotin.tasker.domain.dto.TaskDTO;
+import com.korotin.tasker.mapper.TaskMapper;
+import com.korotin.tasker.service.TaskService;
+import com.korotin.tasker.validator.annotation.ExistingId;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.StreamSupport;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/tasks")
+@Validated
+public class TaskController {
+    private final TaskService taskService;
+    private final TaskMapper mapper;
+
+    @GetMapping("/{taskId}")
+    public OutputTaskDTO getTaskById(@PathVariable @ExistingId(responsible = TaskService.class) UUID taskId) {
+        Task task = taskService.findById(taskId).orElseThrow();
+        return mapper.taskToDTO(task);
+    }
+
+    @PutMapping("/{taskId}")
+    public OutputTaskDTO editTask(@PathVariable @ExistingId(responsible = TaskService.class) UUID taskId,
+                                  @Valid @RequestBody TaskDTO dto) {
+        Task task = taskService.update(taskId, mapper.fromDTO(dto));
+        return mapper.taskToDTO(task);
+    }
+
+    @PostMapping
+    @Parameter(name = "response", hidden = true)
+    public OutputTaskDTO createTask(@Valid @RequestBody TaskDTO dto, HttpServletResponse response) {
+        Task task = taskService.save(mapper.fromDTO(dto));
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        return mapper.taskToDTO(task);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(@PathVariable @ExistingId(responsible = TaskService.class) UUID taskId) {
+        taskService.delete(taskId);
+    }
+
+    @GetMapping
+    public List<OutputTaskDTO> getAllTasks() {
+        return StreamSupport.stream(taskService.findAll().spliterator(), false)
+                .map(mapper::taskToDTO)
+                .toList();
+    }
+}
---
+++ src/main/java/com/korotin/tasker/service/TaskService.java
@@ -0,0 +1,8 @@
+package com.korotin.tasker.service;
+
+import com.korotin.tasker.domain.Task;
+
+import java.util.UUID;
+
+public interface TaskService extends CrudService<Task, UUID> {
+}
