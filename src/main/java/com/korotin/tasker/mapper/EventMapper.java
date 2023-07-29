e38a564665c79401a121e8f5379127cc283583a3
Alexey Korotin <sx12fx@gmail.com>
2023-07-29 21:41:48 +0300

    ref: mapper general configuration spring component model + suppressed warnings

--- src/main/java/com/korotin/tasker/mapper/EventMapper.java
+++ src/main/java/com/korotin/tasker/mapper/EventMapper.java
@@ -4,6 +4,7 @@ import com.korotin.tasker.domain.Event;
 import com.korotin.tasker.domain.Project;
 import com.korotin.tasker.domain.dto.EventDTO;
 import com.korotin.tasker.domain.dto.OutputEventDTO;
+import com.korotin.tasker.mapper.config.MapConfig;
 import com.korotin.tasker.service.ProjectService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.UUID;
 
-@Mapper(uses = UserMapper.class, componentModel = "spring")
+@Mapper(uses = UserMapper.class, config = MapConfig.class)
 public abstract class EventMapper {
 
     @Autowired
